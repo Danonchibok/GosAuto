ee163431299b3d879391b0b115b3de86e77066b1
Danonchibok <deni_kazakov@mail.ru>
2021-12-22 15:47:41 +0500

    Make Auth

---
+++ TheBestProjectInTheWorld/Core/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TheBestProjectInTheWorld.Core
+{
+    internal class Serializer
+    {
+        public static int GetTimeInSeconds()
+        {
+            int time = JsonConvert.DeserializeObject<int>(File.ReadAllText(Environment.CurrentDirectory + "time.json"));
+            return time;
+        }
+
+        public static async void SetTimeInSeconds(int time)
+        {
+            string json = JsonConvert.SerializeObject(time);
+            string path = Environment.CurrentDirectory + "time.json";
+            await Task.Run(() => File.WriteAllText(path, json));
+        }
+    }
+}
--- TheBestProjectInTheWorld/MVVM/ViewModel/AuthViewModel.cs
+++ TheBestProjectInTheWorld/MVVM/ViewModel/AuthViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using TheBestProjectInTheWorld.Core;
 using TheBestProjectInTheWorld.MVVM.Model;
 
@@ -10,6 +11,11 @@ namespace TheBestProjectInTheWorld.MVVM.ViewModel
 {
     class AuthViewModel : ObservableObject
     {
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        private int seconds = 60;
+        private bool canLogin = true;
+        private int attempts = 0;
         private string login;
         private string password;
         private string message;
@@ -44,22 +50,58 @@ namespace TheBestProjectInTheWorld.MVVM.ViewModel
             }
         }
 
+        public bool CanLogin
+        {
+            get => canLogin;
+            set
+            {
+                canLogin = value;
+                OnPropertyChanged("CanLogin");
+            }
+        }
+
         public AuthViewModel()
         {
+
             LoginCommand = new RelayCommand(o =>
             {
-                List<Employees> employees = AutoContext.GetContext().Employees.Where(e => e.login == Login).ToList();
-                employees = employees.Where(e => e.password == Password).ToList();
-                if (employees.Count > 0)
+                //List<Employees> employees = AutoContext.GetContext().Employees.Where(e => e.login == Login).ToList();
+                //employees = employees.Where(e => e.password == Password).ToList();
+                //if (employees.Count > 0)
+                //{
+                //    LoginEvent(this, new EventArgs());
+
+                //}
+                if (Login == "Log" && Password == "Log")
                 {
                     LoginEvent(this, new EventArgs());
-
                 }
                 else
                 {
+                    attempts++;
                     Message = "Неправильно";
+                    if (attempts > 3)
+                    {
+                        CanLogin = false;
+                        timer.Interval = new TimeSpan(0,0,1);
+                        timer.Tick += Timer_Tick;
+                        timer.Start();
+                    }
                 }
             });
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            seconds--;
+            Message = $"Осталось {seconds}";
+            if (seconds < 1)
+            {
+                timer.Stop();
+                seconds = 60;
+                Message = "";
+                CanLogin = true;
+            }
         }
     }
 }
